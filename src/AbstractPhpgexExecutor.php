d24680c07d7faf26382c301eae0750fae7527a11
Halis Duraki <duraki@null.net>
2016-12-18 17:25:18 +0100

    Added: Validate slug / Validate html / Validate zipcode / Validate country / Validate numbers / Validate float / Validate Text

--- src/AbstractPhpgexExecutor.php
+++ src/AbstractPhpgexExecutor.php
@@ -16,6 +16,11 @@ class AbstractPhpgexExecutor implements TypePatternEnums
 		return preg_match(self::PATTERN_URL, $input);
 	}
 
+	public function validateSlug($input)
+	{
+		return preg_match(self::PATTERN_SLUG, strtolower($input));
+	}
+
 	public function validateIPlow($input)
 	{
 		return preg_match(self::PATTERN_IPV4, $input);
@@ -28,32 +33,37 @@ class AbstractPhpgexExecutor implements TypePatternEnums
 
 	public function validateHtml($input)
 	{
+		return preg_match(self::PATTERN_HTML, $input);
+	}
 
+	public function validateHex($input)
+	{
+		return preg_match(self::PATTERN_HEX, strtolower($input));
 	}
 
 	public function validateZipcode($input)
 	{
-
+		return preg_match(self::PATTERN_ZIPCODE, $input);
 	}
 
 	public function validateCountry($input)
 	{
-
+		return preg_match(self::PATTERN_COUNTRY, $input);
 	}
 
-	public function validateNumbers($input)
+	public function validateNumber($input)
 	{
-
+		return preg_match(self::PATTERN_NUMBER, $input);
 	}
 
 	public function validateFloat($input)
 	{
-
+		return preg_match(self::PATTERN_FLOAT, $input);
 	}
 
 	public function validateText($input)
 	{
-
+		return preg_match(self::PATTERN_TEXT, $input);
 	}
 
 	public function validateDate($input)
