ab9c0d40e8fa18972fb6eb48f02baa31870fe99c
Halis Duraki <halis_duraki@outlook.com>
2016-12-18 15:29:09 +0100

    Added: Validation abstract class

---
+++ src/AbstractPhpgexExecutor.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Stackrecord\External\Library\Phpgex;
+
+use Stackrecord\External\Library\Phpgex\TypePatternEnums;
+
+class AbstractPhpgexExecutor implements TypePatternEnums
+{
+	public function validateEmail($input)
+	{
+		
+	}
+
+	public function validateUrl($input)
+	{
+
+	}
+
+	public function validateIPlow($input)
+	{
+
+	}
+
+	public function validateIPhigh($input)
+	{
+		
+	}
+
+	public function validateHtml($input)
+	{
+
+	}
+
+	public function validateZipcode($input)
+	{
+
+	}
+
+	public function validateCountry($input)
+	{
+
+	}
+
+	public function validateNumbers($input)
+	{
+
+	}
+
+	public function validateFloat($input)
+	{
+
+	}
+
+	public function validateText($input)
+	{
+
+	}
+
+	public function validateDate($input)
+	{
+
+	}
+
+	public function validateCreditCardNumber($input)
+	{
+
+	}
+}
---
+++ src/TypePatternEnums.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace  Stackrecord\External\Library\Phpgex;
+
+interface TypePatternEnumbs {
+	
+	const PATTERN_EMAIL		= "";
+
+	const PATTERN_URL		= "";
+
+	const PATTERN_IPV4		= "";
+
+	const PATTERN_IPV6		= "";
+
+	const PATTERN_HTML		= "";
+
+	const PATTERN_ZIPCODE	= "";
+
+	const PATTERN_COUNTRY	= "";
+
+	const PATTERN_NUMBER	= "":
+
+	const PATTERN_FLOAT		= "";
+
+	const PATTERN_TEXT		= "";
+
+	const PATTERN_DATE		= "";
+
+	const PATTERN_CCNO		= "";
+	
+}
