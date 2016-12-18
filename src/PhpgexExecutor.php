46c4d0db849964806f9c0c8a61c708afa383933d
Halis Duraki <halis_duraki@outlook.com>
2016-12-18 15:28:33 +0100

    Added: Main callable methods

---
+++ src/PhpgexExecutor.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Stackrecord\External\Library\Phpgex;
+
+use Stackrecord\External\Library\Phpgex\AbstractPhpgexExecutor;
+
+class PhpgexExecutor extends AbstractPhpgexExecutor 
+{
+	public function isEmail($input)
+	{
+
+	}
+
+	public function isUrl($input)
+	{
+
+	}
+
+	public function isIPvf($input)
+	{
+
+	}
+
+	public function isIPvs($input)
+	{
+
+	}
+
+	public function isHtml($input)
+	{
+
+	}
+
+	public function isZipcode($input)
+	{
+
+	}
+
+	public function isCountry($input)
+	{
+
+	}
+
+	public function isNumbers($input)
+	{
+
+	}
+
+	public function isFloat($input)
+	{
+
+	}
+
+	public function isText($input)
+	{
+
+	}
+
+	public function isDate($input)
+	{
+
+	}
+
+	public function isCreditCardNumber($input)
+	{
+
+	}
+}
