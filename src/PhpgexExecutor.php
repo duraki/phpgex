4bf1ff392da3aece61de6a96418757a216df1b8d
Halis Duraki <duraki@null.net>
2016-12-18 17:25:43 +0100

    Added: Validation methods inherited from abstract

--- src/PhpgexExecutor.php
+++ src/PhpgexExecutor.php
@@ -16,6 +16,11 @@ class PhpgexExecutor extends AbstractPhpgexExecutor
 		return $this->validateUrl($input);
 	}
 
+	public function isSlug($input)
+	{
+		return $this->validateSlug($input);
+	}
+
 	public function isIPv4($input)
 	{
 		return $this->validateIPlow($input);
@@ -28,32 +33,37 @@ class PhpgexExecutor extends AbstractPhpgexExecutor
 
 	public function isHtml($input)
 	{
+		return $this->validateHtml($input);
+	}
 
+	public function isHex($input)
+	{
+		return $this->validateHex($input);
 	}
 
 	public function isZipcode($input)
 	{
-
+		return $this->validateZipcode($input);
 	}
 
 	public function isCountry($input)
 	{
-
+		return $this->validateCountry($input);
 	}
 
-	public function isNumbers($input)
+	public function isNumber($input)
 	{
-
+		return $this->validateNumber($input);
 	}
 
 	public function isFloat($input)
 	{
-
+		return $this->validateFloat($input);
 	}
 
 	public function isText($input)
 	{
-
+		return $this->validateText($input);
 	}
 
 	public function isDate($input)
